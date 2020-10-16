c716e82b955b10577567115ef90eed25a8d24c74
Kevin Chen <kevinch0514@gmail.com>
2020-10-16 09:08:04 -0600

    fixed ability for page to hover and change background image, added CNAME file

--- gallery.js
+++ gallery.js
@@ -1,5 +1,6 @@
 let fs = require('fs');
 
+// Writing New HTML
 let imageFileNames = fs.readdirSync('site/images');
 
 function renderLayout(content) {
@@ -7,53 +8,51 @@ function renderLayout(content) {
 <!DOCTYPE html>
 <html>
   <head>
-    <title>A website!</title>
+    <title>TK Photo Gallery</title>
     <link rel="stylesheet" href="css/main.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
   </head>
   <body>
     <header id="page-header">
-      <h1>Amazing Photo Gallery</h1>
+      <h1>TK Photo Gallery</h1>
     </header>
     <main class="grid" id="content">
       ${content}
     </main>
+    <script>
+      let allImages = document.querySelectorAll('#content img');
+      for (let image of allImages) {
+      image.addEventListener('mouseover', function() {
+        changeBackground(image.src)
+      })
+      }
+    </script>
+    <script src="../gallery.js"></script>
   </body>
 </html>
 `;
 }
 
-// This code is just here to demonstrate how fs.readdirSync works
-
-console.log('sites/images contains the following files:');
-
 function generateGallery(imageFileNames) {
   let html = '';
-  // let photoArray = [];
 
   for (let i = 0; i < imageFileNames.length; i++) {
     let fileName = imageFileNames[i];
-    // photoArray.push(fileName);
-    // console.log(`${i + 1}. ${fileName}`);
-    html += `\t<img src="images/${fileName}>\n`;
-    // console.log(`${i + 1}. ${fileName}`);
+    html += `<img src="images/${fileName}">\n\t\t\t`;
   }
   return html;
 }
 
-console.log(renderLayout(generateGallery(imageFileNames)));
+let newPage = renderLayout(generateGallery(imageFileNames))
+
+fs.writeFileSync('site/index.html', newPage);
+
+console.log('File written successfully.');
 
 
-// // Changing Background
-// function changeBackground(path) {
-//   let result = document.querySelector('#content');
-//   result.style['background-image'] = `url("${path}")`;
-// }
 
-// let allImages = document.querySelectorAll('#content img');
-
-// for (let image of allImages) {
-// image.addEventListener('mouseover', function() {
-//   changeBackground(image.src)
-// })
-// }
+// Changing Background
+function changeBackground(path) {
+  let result = document.querySelector('#content');
+  result.style['background-image'] = `url("${path}")`;
+}
